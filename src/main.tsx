509fd1ab25b909ef807d1c1c9635228844801460
m0r1m0 <yuki.wd7@gmail.com>
2022-09-14 09:06:30 +0900

    feat: add @crxjs/vite-plugin

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const root = document.createElement('div');
+root.id = 'dictee-root'
+document.body.append(root);
+
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
