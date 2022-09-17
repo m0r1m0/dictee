91d461937d5694e864c8e018627b744d2454bb97
m0r1m0 <yuki.wd7@gmail.com>
2022-09-17 15:34:35 +0900

    feat: manager answers in state

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import { Textarea } from "@chakra-ui/react";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "./App.css";
 import { useInterval } from "./useInterval";
 
 function App() {
   const [showElement, setShowElement] = useState(false);
+  const [answer, setAnswer] = useState("");
 
   useInterval(() => {
     setShowElement(document.getElementsByClassName("watch-video").length > 0);
   }, 500);
+
+  const handleTextChange = useCallback(
+    (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+      setAnswer(e.target.value);
+    },
+    []
+  );
 
   if (!showElement) {
     return null;
@@ -16,7 +24,16 @@ function App() {
 
   return (
     <div className="AppContainer">
-      <Textarea fontSize="24" variant="filled" height="100%" placeholder="答えを入力してね！" resize={"none"} isRequired />
+      <Textarea
+        value={answer}
+        onChange={handleTextChange}
+        fontSize="24"
+        variant="filled"
+        height="100%"
+        placeholder="答えを入力してね！"
+        resize={"none"}
+        isRequired
+      />
     </div>
   );
 }
