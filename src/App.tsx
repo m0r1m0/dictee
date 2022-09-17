1aa51508779f1dc951d5ea1071508707a2ad6874
m0r1m0 <yuki.wd7@gmail.com>
2022-09-17 14:32:02 +0900

    feat: add input UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,30 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { useInterval } from "./useInterval";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [showElement, setShowElement] = useState(false);
+
+  useInterval(() => {
+    setShowElement(document.getElementsByClassName("watch-video").length > 0);
+  }, 500);
+
+  if (!showElement) {
+    return null;
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="AppContainer">
+      <input
+        type="text"
+        placeholder="答えを入力してね！"
+        style={{
+          height: 100,
+          width: 600,
+        }}
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
